b63332cb82f57ce5a443da9abb0e9ad3f899991d
serge <linserga@gmail.com>
2014-07-21 17:05:37 +0300

    Finish user edit, update, index, and destory actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
-	factory :user do
-		name "Sergo"
-		email "s@ya.ru"
-		password '123'
-		password_confirmation '123'
-	end
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
+  end
 end
